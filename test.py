812c501a05f72370b6857a7bb9e1d5a0b2a0fb2b
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2018-11-07 17:09:45 -0700

    test file

---
+++ test.py
@@ -0,0 +1,8 @@
+from AudioRecorder import AudioRecorder
+from SpeechRecognizer import SpeechRecognizer
+
+ar =AudioRecorder.AudioRecorder()
+ar.record(5)
+
+sr  =SpeechRecognizer.SpeechRecognizer()
+sr.recognize()
