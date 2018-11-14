6bd91243fa0086ac793d7f9cc99a4c443065e3ee
Carl Mueller <cmuell89@gmail.com>
2018-11-13 18:44:56 -0700

    Deleted files

--- test.py
+++
@@ -1,8 +0,0 @@
-from AudioRecorder import AudioRecorder
-from SpeechRecognizer import SpeechRecognizer
-
-ar =AudioRecorder.AudioRecorder()
-ar.record(5)
-
-sr  =SpeechRecognizer.SpeechRecognizer()
-sr.recognize()
