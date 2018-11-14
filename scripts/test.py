69cc733135c781847eb2bcc3aadb0079908bea00
Carl Mueller <cmuell89@gmail.com>
2018-11-13 19:45:39 -0700

    ROSification

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,15 @@
-from AudioRecorder import AudioRecorder
+#!/usr/bin/env python2
+from cairo_nlp.audio_recording import AudioRecorder
 # from SpeechRecognizer import SpeechRecognizer
 
-ar = AudioRecorder.AudioRecorder()
-ar.record(5)
+
+def main():
+    ar = AudioRecorder()
+    ar.record(5)
 
 # sr  =SpeechRecognizer.SpeechRecognizer()
 # sr.recognize()
 
+
+if __name__ == "__main__":
+    main()
